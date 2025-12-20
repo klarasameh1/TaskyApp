89e3dba5bac263ae34732c439fc36f417364129d
Klara Sameh <klarasameh39@gmail.com>
2025-12-21 00:42:29 +0200

    comments on db

--- lib/database/helper/dp_helper.dart
+++ lib/database/helper/dp_helper.dart
@@ -2,15 +2,21 @@ import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 import '../../models/Task.dart';
 
+/// Database helper class for managing tasks CRUD operations
 class DBHelper {
-  static Database? _db;
+  static Database? _db; //Singleton instance of the database
 
+  //getter for the db
   static Future<Database> get database async {
     if (_db != null) return _db!;
     _db = await _initDB();
     return _db!;
   }
 
+  /// Initializes the DB by:
+  // 1. Getting path to databases folder
+  // 2. Joining database name
+  // 3. Opening/creating the DB with onCreate callback
   static Future<Database> _initDB() async {
     final dbPath = await getDatabasesPath();
     final path = join(dbPath, "tasks.db");
@@ -19,6 +25,7 @@ class DBHelper {
       path,
       version: 1,
       onCreate: (db, version) async {
+        // Create table schema on first creation
         await db.execute('''
           CREATE TABLE tasks (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -56,6 +63,7 @@ class DBHelper {
       );
     }
 
+    // Convert db rows to Task objects
     return res.map((e) => Task.fromMap(e)).toList();
   }
 
